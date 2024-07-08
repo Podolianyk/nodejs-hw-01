3083328ee467b125bcc982a8b5c1fa777416e2f3
Podolianyk <podolianik1988@gmail.com>
2024-07-08 11:21:50 +0200

    update

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const currentContacts = JSON.parse(data);
+    console.log(currentContacts);
+    currentContacts.pop();
+    fs.writeFile(
+      PATH_DB,
+      JSON.stringify(currentContacts, undefined, 2),
+      'utf8',
+    );
+    console.log('Last contact has been deleted');
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
 
 removeLastContact();
